fa220abcb8b145497d01cf959dd7c6eca9f3af68
amanjadon54 <amanjadon54@gmail.com>
2020-04-25 20:02:00 +0530

    Core: exceptional support added

--- src/main/java/com/centime/greeting/handler/RestExceptionHandler.java
+++ src/main/java/com/centime/greeting/handler/RestExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -19,6 +20,22 @@ public class RestExceptionHandler {
     public ResponseEntity<ApiError> handleRestException(
             CustomRuntimeException e, HttpServletRequest request) {
         ApiError apiError = new ApiError(HttpStatus.valueOf(e.getStatus()),e.getMessage(),e.getDeveloperMsg());
+
+        return new ResponseEntity(apiError, apiError.getHttpStatus());
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ApiError> handleJsonException(
+            Exception e, HttpServletRequest request) {
+        ApiError apiError = new ApiError(HttpStatus.UNPROCESSABLE_ENTITY, e.getMessage(), e.getMessage());
+
+        return new ResponseEntity(apiError, apiError.getHttpStatus());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiError> handleRestException(
+            Exception e, HttpServletRequest request) {
+        ApiError apiError = new ApiError(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
 
         return new ResponseEntity(apiError, apiError.getHttpStatus());
     }
